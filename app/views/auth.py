060b6a0af5f18fc3813cf705ecd6e2af53e7acd7
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-01-08 15:37:21 -0500

    created login route

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, jsonify, g, flash, request, render_template
 )
+from sqlalchemy import or_
 from werkzeug.exceptions import abort
 from app.models import db, Users
 import bcrypt
@@ -25,10 +26,13 @@ def register():
     # get password confirm
     password_confirm = request.json.get('password_confirm')   
     # check if user exists in DB 
+    user = Users.query.filter_by(or_(username=username,email=email)).first()
+    """
     user = db.session.execute(
         'SELECT email FROM users WHERE email = :email OR username = :username', {"email":email,"username":username}
     )
-    if user.fetchone():
+    """
+    if user:
         return jsonify({'message':'A user with that email address or username already exists.'})
 
     if password != password_confirm:
@@ -42,15 +46,28 @@ def register():
     )
     db.session.add(user)
     db.session.commit()
-    """
-    result = db.session.execute(
-        'INSERT INTO users (email, username, password) VALUES (:email, :username, :password)', {"email":email,"username":username,"password":password}
-    )
-    print(result)
-    db.session.commit()
-    """
     # instead of returning user, return JWT token
     return jsonify({"user": user.serialize()})
 
 
 # login user
+@bp.route('/login',methods=['GET','POST'])
+def login():
+    # get email and password
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    # check if user exists
+    user = Users.query.filter_by(email=email).first()
+    # if no user exists then return error
+    if user is None:
+        return jsonify({'message':'Invalid username or password'})
+
+    # compare password with hashed password
+    if bcrypt.checkpw(password.encode('utf-8'), user.password):
+        return jsonify({'message':'Login success'})
+
+    return jsonify({'message':'Invalid password'})
+        # if password does not match, return error
+
+        # password matches then return JWT
