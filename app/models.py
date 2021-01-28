90daad37fb08399b44128f29fe81dab6ac1c9b30
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-01-28 12:07:35 -0500

    added password logic to User class.

--- app/models.py
+++ app/models.py
@@ -1,20 +1,31 @@
 from . import db
 import datetime
+import bcrypt
 
 class Users(db.Model):
     __tablename__= "users"
+    
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String,unique=True,nullable=False)
     password = db.Column(db.String,nullable=False)
     username = db.Column(db.String,unique=True,nullable=False)
+
+    def __init__(self,email,password,username):
+        self.username = username
+        self.email = email
+        self.password = self.hash_password(password)
+
+    def is_password_correct(self, input_password):
+        user_pass = self.password.encode('utf-8')
+        encoded_pass = input_password.encode('utf-8')
+        return bcrypt.checkpw(encoded_pass, user_pass)
+
+    def hash_password(self, passwd):
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(passwd.encode('utf-8'),salt).decode('utf-8')
 
     def __repr__(self):
         return f"""
             <User id={self.id} email={self.email} username={self.username} />
             """
     
-    def serialize(self):
-        return {
-            "email":self.email,
-            "username":self.username
-        }
